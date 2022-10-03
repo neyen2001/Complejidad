fc2b2592d5cb0c4abe61ac2106dec0c2abf3d33e
neyen2001 <neyendominguez@gmail.com>
2022-10-02 21:58:42 -0300

    tp
    commi

---
+++ practica ABB/practica ABB/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Neyén
+ * Fecha: 2/10/2022
+ * Hora: 15:56
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace practica_ABB
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
